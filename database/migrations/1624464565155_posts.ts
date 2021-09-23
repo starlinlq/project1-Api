ae70c4ff7908d1764bde79e90359dca8a2108177
starlinlq <67881697+starlinlq@users.noreply.github.com>
2021-09-23 07:15:52 -0500

    x

--- database/migrations/1624464565155_posts.ts
+++ database/migrations/1624464565155_posts.ts
@@ -12,7 +12,7 @@ export default class Posts extends BaseSchema {
       table.text('story').notNullable()
       table.string('description', 500).notNullable()
       table.string('photo_url', 1000).notNullable()
-      table.integer('user_id', 150).references('id').inTable('users').onDelete('CASCADE')
+      table.integer('user_id', 150).references('id').inTable('users')
       table.string('category_title')
       table.integer('category_id')
     })
